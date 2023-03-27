63fe2b03eabc2e1da99d1604dd48dcd79e6fdc76
staniboy <taslaw.wysocki@gmail.com>
2023-03-27 16:30:36 +0400

    feat: player draw method

--- script.js
+++ script.js
@@ -1,38 +1,53 @@
-window.addEventListener('load', function() {
-    /** @type {HTMLCanvasElement} */
-    const canvas = this.document.getElementById('canvas1');
-    const ctx = canvas.getContext('2d');
-    canvas.width = 1280;
-    canvas.height = 720;
+window.addEventListener("load", function () {
+  /** @type {HTMLCanvasElement} */
+  const canvas = this.document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
+  canvas.width = 1280;
+  canvas.height = 720;
 
-    class Player {
-        constructor(game) {
-            this.game = game;
-        }
-        draw(context) {
-            context.beginPath();
-            context.arc(100, 100, 50, 0, MATH.PI * 2);
-            context.fill();
-        }
+  ctx.fillStyle = "white";
+  ctx.lineWidth = 3;
+  ctx.strokeStyle = "white";
+
+  class Player {
+    constructor(game) {
+      this.game = game;
+      this.collisionX = this.game.width * 0.5;
+      this.collisionY = this.game.height * 0.5;
+      this.collisionRarius = 30;
     }
-
-    class Game {
-        constructor(canvas) {
-            this.canvas = canvas;
-            this.width = this.canvas.width;
-            this.height = this.canvas.height;
-
-            this.player = new Player(this);
-        }
-        render(context) {
-            this.player.draw(context);
-        }
+    draw(/** @type {CanvasRenderingContext2D} */ context) {
+      context.beginPath();
+      context.arc(
+        this.collisionX,
+        this.collisionY,
+        this.collisionRarius,
+        0,
+        Math.PI * 2
+      );
+      context.save();
+      context.globalAlpha = 0.5;
+      context.fill();
+      context.restore();
+      context.stroke();
     }
+  }
 
-    const game = new Game(canvas);
-    console.log(game);
-
-    function animate() {
-
+  class Game {
+    constructor(canvas) {
+      this.canvas = canvas;
+      this.width = this.canvas.width;
+      this.height = this.canvas.height;
+      this.player = new Player(this);
     }
-})
+    render(context) {
+      this.player.draw(context);
+    }
+  }
+
+  const game = new Game(canvas);
+  game.render(ctx);
+  console.log(game);
+
+  function animate() {}
+});
