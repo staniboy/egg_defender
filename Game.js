16731b1d6253cf6f33d88f6a6949d788d6f98d17
staniboy <taslaw.wysocki@gmail.com>
2023-03-28 13:20:33 +0400

    feat: checkCollision method

--- Game.js
+++ Game.js
@@ -72,6 +72,14 @@ export default class Game {
       attempts++;
     }
   }
+  /** @returns {boolean} */
+  checkCollision(a, b) {
+    const dx = a.collisionX - b.collisionX;
+    const dy = a.collisionY - b.collisionY;
+    const distance = Math.hypot(dy, dx);
+    const sumOfRadii = a.collisionRadius + b.collisionRadius;
+    return distance < sumOfRadii;
+  }
   init() {
     this.populateObstacles();
   }
