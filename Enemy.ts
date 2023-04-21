9efee210cc2d254531d27ec463451be4ffe4f12f
staniboy <stanley.v@outlook.com>
2023-04-21 20:20:04 +0400

    feat: draw method

--- Enemy.ts
+++ Enemy.ts
@@ -2,11 +2,12 @@ import type Game from "./Game";
 import GameObject from "./GameObject";
 
 export default class Enemy extends GameObject {
-  speedX: number = Math.random() * 5 + 0.5;
+  frameX: number = 0;
+  frameY: number = Math.floor(Math.random() * 4);
+  speedX: number = Math.random() * 3 + 0.5;
   constructor(game: Game) {
-    super(game, "toad");
+    super(game, "toads");
     this.collisionRadius = 30;
-
 
     this.spriteWidth = 140;
     this.spriteHeight = 260;
@@ -20,12 +21,41 @@ export default class Enemy extends GameObject {
     this.spriteX = this.collisionX - this.width * 0.5;
     this.spriteY = this.collisionY - this.height * 0.5 - 25;
   }
+  draw(context: CanvasRenderingContext2D) {
+    context.drawImage(
+      this.image,
+      this.frameX * this.spriteWidth,
+      this.frameY * this.spriteHeight,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.spriteX,
+      this.spriteY,
+      this.width,
+      this.height
+    );
+    if (this.game.debug) {
+      context.beginPath();
+      context.arc(
+        this.collisionX,
+        this.collisionY,
+        this.collisionRadius,
+        0,
+        Math.PI * 2
+      );
+      context.save();
+      context.globalAlpha = 0.5;
+      context.fill();
+      context.restore();
+      context.stroke();
+    }
+  }
   update() {
     this.collisionX -= this.speedX;
     this.spriteX = this.collisionX - this.width * 0.5;
     this.spriteY = this.collisionY - this.height + 40;
     // Reposition enemy on canvas once it left the canvas
-    if (this.spriteX + this.width < 0) {
+    if (!this.game.gameOver && this.spriteX + this.width < 0) {
+      this.frameY = Math.floor(Math.random() * 4);
       this.collisionX = this.game.width;
       this.collisionY =
         this.game.topMargin +
